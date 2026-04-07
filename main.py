25213ea3b38748ab7d368b88f3fda431de44f1b9
NIck-Nick0 <islamomar54444@gmail.com>
2026-04-07 19:47:06 +0200

    turn

--- main.py
+++ main.py
@@ -1,60 +1,45 @@
 class Hero: 
-    def __init__(self, id, name, atk, ar, mr,max_hel, atkT="ad"):
-        # المتغيرات الأساسية (التي تبدأ بـ _ لتعبر عن أنها خاصة)
+    def __init__(self, id, name, atk, ar, max_hel ):
         self._id = id
         self._name = name
         self._atk = atk
         self._ar = ar
-        self._mr = mr
         self._max_hel = max_hel
         self._c_hel = max_hel
-        self._atkT = atkT
         self._ex = True
+        self._ap =3
+    # --- القراء (Getters) ---
+    @property
+    def id(self): return self._id
 
     @property
-    def id(self):
-        return self._id
+    def name(self): return self._name
 
     @property
-    def name(self):
-        return self._name
+    def atk(self): return self._atk
 
     @property
-    def cost(self):
-        return self._cost
+    def ar(self): return self._ar
 
     @property
-    def ex(self):
-        return self._ex
-
-    @property
-    def atk(self):
-        return self._atk
-
-    @property
-    def ar(self):
-        return self._ar
-
-    @property
-    def mr(self):
-        return self._mr
-
-    @property
-    def c_hel(self):
-        return self._c_hel
+    def c_hel(self): return self._c_hel
     
     @property
-    def max_hel(self):
-        return self._max_hel
+    def max_hel(self): return self._max_hel
     
     @property
-    def atkT(self):
-        return self._atkT
+    def ex(self): return self._ex
+    
+    @property
+    def ap(self): return self._ap
 
-    # --- المعدلون (Setters) ---
-    @cost.setter
-    def cost(self, value):
-        self._cost = value
+    @property
+    def is_alive(self):
+        return self.c_hel >= 0
+
+
+    # -------------------------  (setter) ---------------------------
+
 
     @atk.setter
     def atk(self, value):
@@ -64,150 +49,124 @@ class Hero:
     def ar(self, value):
         self._ar = value
 
-    @mr.setter
-    def mr(self, value):
-        self._mr = value
-
     @c_hel.setter
-    def max_hel(self, value):
+    def c_hel(self, value): 
         self._c_hel = value
 
-
     @max_hel.setter
-    def max_hel(self, value):
+    def max_hel(self, value): 
         self._max_hel = value
 
 
-    def attck (self :object,target :object ) :
-        target._hel
-        damage = self.atk - target.ar
+    def Battck (self :object,target :object ) :
+        if self._ap >= 3 :
+            target.c_hel
+            damage = self.atk - target.ar
+            
+            if damage < 0: damage = 0
+            
+            target.c_hel -= damage
+            print(f"{self.name} deals {damage} damage to {target.name}")
+            self._ap -= 3
+        else : 
+            print("mo ap point to use this action")
+
+    # def attck (self :object,target :object ) :
+    #     if self._ap >= 3 :
+    #         target.c_hel
+    #         damage = self.atk - target.ar
+            
+    #         if damage < 0: damage = 0
+            
+    #         target.c_hel -= damage
+    #         print(f"{self.name} deals {damage} damage to {target.name}")
+    #         self._ap -= 3
+    #     else : 
+    #         print("mo ap point to use this action")
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Card(Hero):
+    def __init__(self, id, name, cost, atk, ar, max_hel):
+        super().__init__(id, name, atk, ar, max_hel)
         
-        # لضمان أن الضرر لا يكون سالباً (يعالج الخصم)
-        if damage < 0: damage = 0
-        
-        target.hel -= damage
-        print(f"{self.name} deals {damage} damage to {target.name}")
-
-
-# def __init__(self, id, name, cost, atk, ar, mr, hel, spell_power):
-#         # استدعاء الـ __init__ الخاص بالكلاس الأب
-#         super().__init__(id, name, cost, atk, ar, mr, hel)
-#         self.spell_power = spell_power
-
-class Card(Hero) :
-    def __init__(self, id, name, cost, atk, ar, mr,max_hel, atkT="ad"):
-        # المتغيرات الأساسية (التي تبدأ بـ _ لتعبر عن أنها خاصة)
-        super().__init__(id, name, atk, ar, mr, max_hel)
         self._cost = cost
-        
-        self._atkT = atkT
-        self._atkT = atkT
-
-        self._ex = True
 
     # --- القراء (Getters) ---
-    # @property
-    # def id(self):
-    #     return self._id
-
-    # @property
-    # def name(self):
-    #     return self._name
-
     @property
     def cost(self):
         return self._cost
-
-    @property
-    def ex(self):
-        return self._ex
-
-    # @property
-    # def atk(self):
-    #     return self._atk
-
-    # @property
-    # def ar(self):
-    #     return self._ar
-
-    # @property
-    # def mr(self):
-    #     return self._mr
-
-    # @property
-    # def hel(self):
-    #     return self._hel
-
-    @property
-    def atkT(self):
-        return self._atkT
 
     # --- المعدلون (Setters) ---
     @cost.setter
     def cost(self, value):
         self._cost = value
-
-    # @atk.setter
-    # def atk(self, value):
-    #     self._atk = value
-
-    # @ar.setter
-    # def ar(self, value):
-    #     self._ar = value
-
-    # @mr.setter
-    # def mr(self, value):
-    #     self._mr = value
-
-    # @hel.setter
-    # def hel(self, value):
-    #     self._hel = value
-
-
-
     
 
 
 
 
+ 
 
 
 
 
 
 
+mon1 = Card(1, "goblin", cost=3, atk=5, ar=2, max_hel=20)
+mon2 = Card(2, "Dragon", cost=7, atk=15, ar=3, max_hel=30)
+hero1= Hero(3, "Knight", atk=3, ar=3, max_hel=100)
+hero2 = Hero(4, "Knight", atk=3, ar=3,  max_hel=100)
+
+
+print(mon1.__dict__)
+print(mon2.__dict__)
+print(hero1.__dict__)
+print(hero2.__dict__)
+
+mon1.Battck(mon2)
+mon1.Battck(mon2)
+
+
+
+print(mon1.__dict__)
+print(mon2.__dict__)
+
+
+# add deak 
+while hero1.is_alive and hero2.is_alive :
+    turn = 0 
+    hero1.ap = hero1.ap + turn
+    while hero1.ap > 0 :
+        action = input("play")
+
+
+        ...
 
 
 
 
+    while hero1.ap > 0 :
+
+        ...
+
+
+    turn += 1 
 
 
 
+    # x=input()
+    # if x == "1" : 
+    #     pass
 
-
-
-
-    def attak(self, n):
-        if self._size - n < 0 :
-                 raise ValueError
-
-        self._size = self._size  - n
-        return self._size  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-mon1 =Card(12,"stack",5,10,3,3,20)
-print(mon1.cost)
+    # hero1.ap,hero2.ap += 3
